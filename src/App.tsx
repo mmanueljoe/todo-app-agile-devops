11b6ee84932145d28def1cf214e7ac1f590c7574
mmanueljoe <emmanuelletsu18@gmail.com>
2026-02-16 14:42:26 +0000

    initial: create vite react project structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+      <div>
+        <h1 className="text-2xl font-bold">Todo App</h1>
+        <input type="text" placeholder="Add a new todo" className="border border-gray-300 rounded-md p-2" />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Add</button>
+      </div>
+  )
+}
+
+export default App
